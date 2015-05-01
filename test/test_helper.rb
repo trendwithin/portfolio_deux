072fd1e7311391af9ec61d9d35eb1985b0c6dfd5
Michael Becco <e4e6d4d5@gmail.com>
2015-05-01 11:47:45 -0700

    Add Minitest

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,21 @@
-require 'codeclimate-test-reporter'
+require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
+ENV["RAILS_ENV"] = "test"
+require File.expand_path("../../config/environment", __FILE__)
+require "rails/test_help"
+require "minitest/rails"
+require "minitest/pride"
+require "minitest/reporters"
+require "minitest/rails/capybara"
+
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+    ActiveRecord::Migration.check_pending!
+
+    # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
+  #
+  # Note: You'll currently still have to declare fixtures explicitly in integration tests
+  # -- they do not yet inherit this setting
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
