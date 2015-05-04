1736d1420680a8743582c12ae746a0419019f00f
Michael Becco <e4e6d4d5@gmail.com>
2015-05-04 15:25:33 -0700

    Refactor Policy Test

--- test/features/policy/project_policy_test.rb
+++ test/features/policy/project_policy_test.rb
@@ -2,18 +2,14 @@ require "test_helper"
 
   feature 'Guest Tries Fills Out A Project' do
     scenario 'Guest goes to path for new project' do
-    user = users(:guest)
-    project = projects(:portfolio)
-    project_policy = ProjectPolicy.new(user, project)
-    assert !project_policy.new?
+      project_policy = guest_policy
+      assert !project_policy.new?
   end
 end
 
   feature 'Guest Tries to Create a Project' do
     scenario 'Guess Passes Data to Create' do
-      user = users(:guest)
-      project = projects(:portfolio)
-      project_policy = ProjectPolicy.new(user, project)
+      project_policy = guest_policy
       assert !project_policy.create?
     end
   end
